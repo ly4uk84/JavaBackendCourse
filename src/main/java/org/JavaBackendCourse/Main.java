b51be20effec7ea36776c95df1b67d481282baf2
Ly4uk <ly4uk84@gmail.com>
2025-10-13 23:42:39 +0400

    observer

--- src/main/java/org/JavaBackendCourse/Main.java
+++ src/main/java/org/JavaBackendCourse/Main.java
@@ -1,7 +1,80 @@
 package org.JavaBackendCourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+interface StringBuilderObserver {
+    void onChange(String newValue);
+}
+
+
+class ObservableStringBuilder {
+    private final StringBuilder builder = new StringBuilder();
+    private final List<StringBuilderObserver> observers = new ArrayList<>();
+
+
+    public void addObserver(StringBuilderObserver observer) {
+        observers.add(observer);
+    }
+
+
+    public void removeObserver(StringBuilderObserver observer) {
+        observers.remove(observer);
+    }
+
+
+    private void notifyObservers() {
+        String currentValue = builder.toString();
+        for (StringBuilderObserver observer : observers) {
+            observer.onChange(currentValue);
+        }
+    }
+
+
+    public ObservableStringBuilder append(String str) {
+        builder.append(str);
+        notifyObservers();
+        return this;
+    }
+
+    public ObservableStringBuilder insert(int offset, String str) {
+        builder.insert(offset, str);
+        notifyObservers();
+        return this;
+    }
+
+    public ObservableStringBuilder delete(int start, int end) {
+        builder.delete(start, end);
+        notifyObservers();
+        return this;
+    }
+
+    public ObservableStringBuilder replace(int start, int end, String str) {
+        builder.replace(start, end, str);
+        notifyObservers();
+        return this;
+    }
+
+    public ObservableStringBuilder reverse() {
+        builder.reverse();
+        notifyObservers();
+        return this;
+    }
+
+    public String toString() {
+        return builder.toString();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        ObservableStringBuilder observ = new ObservableStringBuilder();
 
+        observ.addObserver(newValue -> System.out.println("Observ1: new " + newValue));
+        observ.addObserver(newValue -> System.out.println("Observ2: change " + newValue));
+
+        observ.append("hello");
+        observ.append(" world");
     }
 }
